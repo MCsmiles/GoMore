63ed47a2fa5a0251255ddd213dca56def7cd77e7
C微笑😄 <512599005@qq.com>
2019-07-08 09:42:44 +0800

    1

--- jz_prd/CA/薪水薪事/files/工作台/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
-var b="url",c="工作台.html",d="generationDate",e=new Date(1555466638342.98),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="73e309de5dec4bfeacdbd76a1ac638a7",q="type",r="Axure:Page",s="name",t="工作台",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="9b5522fd22094a1c829616c07efdd6be",Y="label",Z="friendlyType",ba="Image",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=1280,bj="height",bk=800,bl="imageOverrides",bm="images",bn="normal~",bo="images/工作台/u66.jpg",bp="masters",bq="objectPaths",br="9b5522fd22094a1c829616c07efdd6be",bs="scriptId",bt="u66";
-return _creator();
-})());
